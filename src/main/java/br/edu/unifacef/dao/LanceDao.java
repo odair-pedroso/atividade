ab4903be3004aba10cd292cf8f63b95f4c905ff7
odair-pedroso <odairpedroso90@gmail.com>
2021-10-03 12:52:56 -0300

    Leilao e Lance DAO

---
+++ src/main/java/br/edu/unifacef/dao/LanceDao.java
@@ -0,0 +1,32 @@
+package br.edu.unifacef.dao;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import br.edu.unifacef.model.Lance;
+import br.edu.unifacef.model.Leilao;
+
+
+
+@Repository
+public class LanceDao {
+	
+	private EntityManager em;
+
+	@Autowired
+	public LanceDao(EntityManager em) {
+		this.em = em;
+	}
+	public void salvar(Lance lance) {
+		em.persist(lance);
+	}
+
+	public Lance buscarMaiorLanceDoLeilao(Leilao leilao) {
+		return em.createQuery("SELECT l FROM Lance l WHERE l.valor = (SELECT MAX(lance.valor) FROM Lance lance)", Lance.class)
+				.setParameter("leilao", leilao)
+				.getSingleResult();
+	}
+
+}
