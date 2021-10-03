9b62434ee163c1c849878cb81c2dc502745768ba
odair-pedroso <odairpedroso90@gmail.com>
2021-10-03 13:17:22 -0300

    testes finalizar leilao service

---
+++ src/test/java/br/edu/unifacef/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,89 @@
+package br.edu.unifacef.service;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.edu.unifacef.dao.LeilaoDao;
+import br.edu.unifacef.model.Lance;
+import br.edu.unifacef.model.Leilao;
+import br.edu.unifacef.model.Usuario;
+
+
+class FinalizarLeilaoServiceTest {
+
+private FinalizarLeilaoService service ;
+	
+	@Mock
+	private LeilaoDao leilaoDao;
+	
+	@Mock
+	private EnviadorDeEmails enviadorDeEmails;
+	
+	@BeforeEach
+	public void beforeEach() {
+		MockitoAnnotations.initMocks(this);
+		this.service = new FinalizarLeilaoService(leilaoDao,enviadorDeEmails);		
+	}	
+	
+	private List<Leilao> leiloes(){	
+		
+		List<Leilao> lista = new ArrayList<>();		
+		Leilao leilao = new Leilao ("Celular",new BigDecimal("500"),new Usuario("Fulano"));
+		
+		Lance primeiro = new Lance (new Usuario ("Beltrano"),new BigDecimal("600"));
+		Lance segundo = new Lance (new Usuario ("Ciclano"),new BigDecimal("900"));
+		
+		leilao.propoe(primeiro);
+		leilao.propoe(segundo);	
+				
+		lista.add(leilao);		
+		
+		return lista;	
+		
+		
+	}
+	
+	@Test
+	void deveriaFinalizarUmLeilao() {
+		List<Leilao> leiloes = leiloes();
+		
+		Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+		
+		service.finalizarLeiloesExpirados();
+		
+		Leilao leilao = leiloes.get(0);
+		assertTrue(leilao.isFechado());
+		assertEquals(new BigDecimal("900"),leilao.getLanceVencedor().getValor());
+		
+		Mockito.verify(leilaoDao).salvar(leilao);
+		
+		
+	}
+	
+	@Test
+	void deveriaEnviarEmailParaVencedorDoLeilao() {
+		List<Leilao> leiloes = leiloes();
+		
+		Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+		
+		service.finalizarLeiloesExpirados();
+		
+		Leilao leilao = leiloes.get(0);
+		Lance lanceVencedor = leilao.getLanceVencedor();
+		
+		
+		Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);		
+		
+	}
+
+}
